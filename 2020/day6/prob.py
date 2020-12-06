c59a52ccc18e700691250f297c02d44618e5c4f9
Mark Reid <mreid@mozilla.com>
2020-12-06 10:51:04 -0400

    solve day 6 part 2

--- 2020/day6/prob.py
+++ 2020/day6/prob.py
@@ -1,3 +1,4 @@
+import collections
 import logging
 import re
 import sys
@@ -23,3 +24,28 @@ for line in my_input:
 total_yeses += len(current)
 
 logging.info("Found {} yeses".format(total_yeses))
+
+def count_common_answers(d):
+  group_size = d.get('group_size')
+  common_answers = 0
+  for k, v in d.items():
+    if k == 'group_size':
+      continue
+    elif v == group_size:
+      common_answers += 1
+  return common_answers
+
+common_yeses = 0
+current = collections.defaultdict(int)
+
+for line in my_input:
+  if line == '':
+    common_yeses += count_common_answers(current)
+    current = collections.defaultdict(int)
+  else:
+    current['group_size'] += 1
+    for a in line:
+      current[a] += 1
+
+common_yeses += count_common_answers(current)
+logging.info("Found {} common yeses".format(common_yeses))
