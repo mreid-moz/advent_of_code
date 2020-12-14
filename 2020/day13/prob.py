bda6df603c49d35771c1c149dac0071ad9c40ce5
Mark Reid <mreid@mozilla.com>
2020-12-14 09:59:42 -0400

    Brute force day 13 part 2

--- 2020/day13/prob.py
+++ 2020/day13/prob.py
@@ -1,6 +1,8 @@
 import logging
 import sys
 
+from math import gcd
+
 logging.basicConfig(level=logging.DEBUG)
 
 input_file = 'input'
@@ -36,3 +38,40 @@ time_delta = closest_time - target_time
 bus_product = bus_num * time_delta
 logging.info("Part 1: Bus {} leaves closest to {} (at {}), {} x {} = {}".format(
   bus_num, target_time, closest_time, bus_num, time_delta, bus_product))
+
+def lcm(a, b):
+    """Find the least common multiple"""
+    return abs(a * b) // gcd(a, b)
+
+letters = 'abcdefghijklmnopqrst'
+
+equations = list()
+num_equations = 0
+for i, v in enumerate(buses):
+  if v == 'x':
+    continue
+  num_equations += 1
+  equations.append((i, int(v)))
+  logging.debug("Equation {}: {}{} - {} = t".format(num_equations, v, letters[num_equations-1], i))
+
+base = equations[0][1]
+equation_offset = 1
+m = 1
+while True:
+  c = base * m
+  good = True
+  if m % 1000000 == 0:
+    logging.debug("Checking multiple {} * {}".format(m, base))
+  for offset, val in equations[equation_offset:]:
+    if (c + offset) % val != 0:
+      good = False
+      break
+  if good:
+    logging.info("part 2: Timestamp was {}".format(base * m))
+    break
+  m += 1
+
+# Equation 1: 17a + 0 = t
+# Equation 2: 13b + 2 = t
+# Equation 3: 19c + 3 = t
+# t = 3417
