27670c7124b9c62610549659a0579b8ebc4e124a
Mark Reid <mreid@mozilla.com>
2020-12-14 17:35:37 -0400

    Solve day 13 part 2

--- 2020/day13/prob.py
+++ 2020/day13/prob.py
@@ -3,7 +3,7 @@ import sys
 
 from math import gcd
 
-logging.basicConfig(level=logging.DEBUG)
+logging.basicConfig(level=logging.INFO)
 
 input_file = 'input'
 if len(sys.argv) >= 2:
@@ -39,54 +39,18 @@ bus_product = bus_num * time_delta
 logging.info("Part 1: Bus {} leaves closest to {} (at {}), {} x {} = {}".format(
   bus_num, target_time, closest_time, bus_num, time_delta, bus_product))
 
-def lcm(a, b):
-    """Find the least common multiple"""
-    return abs(a * b) // gcd(a, b)
-
-letters = 'abcdefghijklmnopqrst'
-
 equations = list()
-num_equations = 0
 for i, v in enumerate(buses):
   if v == 'x':
     continue
-  num_equations += 1
   equations.append((i, int(v)))
-  logging.debug("Equation {}: {}{} - {} = t".format(num_equations, v, letters[num_equations-1], i))
 
-base = equations[0][1]
-equation_offset = 1
-m = 1
-m_step = 1
-last_match = base
-last_match_length = 1
-while True:
-  c = base * m
-  good = True
-  if m % 1000000 == 0:
-    logging.info("Checking multiple {} * {}. m_step is {}".format(m, base, m_step))
-  current_match_length = 0
-  for offset, val in equations[equation_offset:]:
-    if (c + offset) % val != 0:
-      good = False
-      break
-    else:
-      current_match_length += 1
-      if last_match is None:
-        last_match = c
-        last_match_length = current_match_length
-      #TODO: fix this trash
-      # elif current_match_length > last_match_length:
-      #  base = c - last_match
-      #  m = 2
-      #  logging.debug("Setting step size to {}".format(base))
-      #  last_match = c
-      #  last_match_length = current_match_length
-      logging.debug("Found a value {} ({} * {}) that is {} % {} and {} % {}".format(c, base, m, offset, val, c % base, base))
+total = 1
+increment = 1
+for offset, bus_value in equations:
+  while (total + offset) % bus_value != 0:
+    logging.debug("Incrementing total by {} from {} to {}".format(increment, total, total+increment))
+    total += increment
+  increment *= bus_value
+logging.info("Part 2: timestamp was {}".format(total))
 
-      #if val > m_step:
-      #  m_step = val
-  if good:
-    logging.info("part 2: Timestamp was {}".format(c))
-    break
-  m += m_step
