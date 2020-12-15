a780de459cad99ce1e1b00ca328dc9dcff158ab0
Mark Reid <mreid@mozilla.com>
2020-12-15 09:58:00 -0400

    Solve Day 15 part 2

--- 2020/day15/prob.py
+++ 2020/day15/prob.py
@@ -17,25 +17,38 @@ def rseek(haystack, needle):
       return location
   return None
 
-numbers = [int(s) for s in my_input[0].split(',')]
-num_iterations = 2020
 
-for i in range(num_iterations):
-  if i % 5000 == 0:
-    logging.info("Looking for number #{}".format(i+1))
-  if i < (len(numbers)):
-    logging.debug("starting value: {}".format(numbers[i]))
-    continue
-  last_number = numbers[i-1]
-  logging.debug("Last number was {}, looking for it in {}".format(last_number, numbers[:-1]))
-  location = rseek(numbers[:-1], last_number)
-  if location is None:
-    logging.debug("This was the first time we saw it")
-    next_number = 0
-  else:
-    logging.debug("We last saw it at index {}".format(location))
-    next_number = i - location - 1
 
-  logging.debug("Next number {} is {}".format(i+1, next_number))
-  numbers.append(next_number)
-logging.info("Part 1: number {} was {}".format(i+1, numbers[-1]))
+def run(num_iterations):
+  numbers = [int(s) for s in my_input[0].split(',')]
+  last_seen_cache = {}
+  penultimate_seen_cache = {}
+  for i in range(num_iterations):
+    if i % 5000 == 0:
+      logging.info("Looking for number #{}".format(i+1))
+    else:
+      logging.debug("Looking for number #{}".format(i+1))
+    if i < (len(numbers)):
+      logging.debug("starting value: {}".format(numbers[i]))
+      penultimate_seen_cache[numbers[i]] = last_seen_cache.get(numbers[i])
+      last_seen_cache[numbers[i]] = i
+      continue
+    last_number = numbers[i-1]
+    location = penultimate_seen_cache.get(last_number)
+    if location is None:
+      logging.debug("This was the first time we saw {}".format(last_number))
+      next_number = 0
+    else:
+      logging.debug("We last saw it at index {}".format(location))
+      next_number = i - location - 1
+
+    logging.debug("Next number {} is {}".format(i+1, next_number))
+    numbers.append(next_number)
+    penultimate_seen_cache[next_number] = last_seen_cache.get(next_number)
+    last_seen_cache[next_number] = i
+  return (i+1, numbers[-1])
+
+n, val = run(2020)
+logging.info("Part 1: number {} was {}".format(n, val))
+n, val = run(30000000)
+logging.info("Part 2: number {} was {}".format(n, val))
