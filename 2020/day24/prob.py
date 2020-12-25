468884a6ea8e2dd79d78ea27452d9fc6dfb32ddc
Mark Reid <mreid@mozilla.com>
2020-12-25 17:11:17 -0400

    Solve day 24 part 1

--- 2020/day24/prob.py
+++ 2020/day24/prob.py
@@ -17,6 +17,15 @@ RELATIVE_DIRS = {
   'sw': 'ne',
   'nw': 'se',
 }
+DIR_COORDS = {
+  # d:  [ x,  y,  z]
+  'e':  [ 1, -1,  0],
+  'w':  [-1,  1,  0],
+  'ne': [ 1,  0, -1],
+  'nw': [ 0,  1, -1],
+  'se': [ 0, -1,  1],
+  'sw': [-1,  0,  1]
+}
 
 input_file = 'input'
 if len(sys.argv) >= 2:
@@ -25,43 +34,28 @@ with open(input_file) as fin:
   my_input = [l.strip().replace(' ', '') for l in fin.readlines()]
 
 class HexTile:
-  def __init__(self, path):
-    self.neighbours = {}
+  def __init__(self, x, y, z):
+    self.x = x
+    self.y = y
+    self.z = z
     self.side = WHITE
-    self.path = path
+
+  def key(self):
+    return "{},{},{}".format(self.x, self.y, self.z)
 
   def flip(self):
     if self.side == WHITE:
       self.side = BLACK
-      logging.debug("Flipping tile {} from {} to {}".format(self.get_path(), WHITE, BLACK))
+      logging.debug("Flipping tile {} from {} to {}".format(self.key(), WHITE, BLACK))
     else:
       self.side = WHITE
-      logging.debug("Flipping tile {} from {} to {}".format(self.get_path(), BLACK, WHITE))
-
-  def get_path(self):
-    return ' '.join(self.path)
-
-  def get_neighbour_sides(self):
-    sides = {}
-    for d in DIRECTIONS:
-      if d in self.neighbours:
-        sides[d] = self.neighbours[d].side
-      else:
-        sides[d] = 'x'
-    return sides
-
-  def print(self, indent=0):
-    logging.debug("Path: {}".format(self.get_path()))
-    sides = self.get_neighbour_sides()
-    logging.debug(" {} {} ".format(sides['nw'], sides['ne']))
-    logging.debug("{} {} {}".format(sides['w'], self.side, sides['e']))
-    logging.debug(" {} {} ".format(sides['sw'], sides['se']))
+      logging.debug("Flipping tile {} from {} to {}".format(self.key(), BLACK, WHITE))
 
 class HexGrid:
   def __init__(self):
-    self.centre = HexTile([])
+    self.centre = HexTile(0, 0, 0)
     self.all_tiles = {}
-    self.all_tiles[self.centre.get_path()] = self.centre
+    self.all_tiles[self.centre.key()] = self.centre
 
   def parse_directions(self, dir_str):
     dir_list = list(dir_str)
@@ -76,74 +70,36 @@ class HexGrid:
       dirs.append(c)
     return dirs
 
-  def minify_directions(self, dirs):
-    logging.debug("minfying {}".format(' '.join(dirs)))
-    dir_counts = defaultdict(int)
-    for d in dirs:
-      dir_counts[d] += 1
+  def flip(self, directions):
+    xd, yd, zd = [0, 0, 0]
+    for d in directions:
+      x, y, z = DIR_COORDS[d]
+      xd += x
+      yd += y
+      zd += z
 
-    while(dir_counts['e'] > 0 and dir_counts['w'] > 0):
-      dir_counts['e'] -= 1
-      dir_counts['w'] -= 1
-    while(dir_counts['se'] > 0 and dir_counts['nw'] > 0):
-      dir_counts['se'] -= 1
-      dir_counts['nw'] -= 1
-    while(dir_counts['sw'] > 0 and dir_counts['ne'] > 0):
-      dir_counts['sw'] -= 1
-      dir_counts['ne'] -= 1
-    minified = []
-    for k in sorted(dir_counts.keys()):
-      v = dir_counts[k]
-      for i in range(v):
-        minified.append(k)
-    logging.debug("minified to: {}".format(' '.join(minified)))
-    return minified
-
-  def flip(self, tile, directions, already_processed_dirs=[]):
-    #logging.debug("Flipping tile {} / {}".format(" ".join(already_processed_dirs), " ".join(directions)))
-    if len(directions) == 0:
-      tile.flip()
+    t = HexTile(xd, yd, zd)
+    tk = t.key()
+    if tk in self.all_tiles:
+      self.all_tiles[tk].flip()
     else:
-      d = directions[0]
-      if d not in tile.neighbours:
-        next_tile = HexTile(already_processed_dirs + [d])
-        next_tile.neighbours[RELATIVE_DIRS[d]] = tile
-        tile.neighbours[d] = next_tile
-        self.all_tiles[next_tile.get_path()] = next_tile
-        #next_tile.flip()
-      self.flip(tile.neighbours[d], directions[1:], already_processed_dirs + [d])
+      t.flip()
+      self.all_tiles[tk] = t
 
   def print(self):
-    max_path_length = 0
     for tile in self.all_tiles.values():
-      if len(tile.path) > max_path_length:
-        max_path_length = len(tile.path)
-
-    for i in range(max_path_length + 1):
-      #logging.debug("Printing paths of length {}".format(i))
-      for k in sorted(self.all_tiles.keys()):
-        t = self.all_tiles[k]
-        #logging.debug("Key: {}".format(k))
-        if len(t.path) == i:
-          t.print()
-
-
-def test():
-  hg = HexGrid()
-  dirs = hg.parse_directions('esenee')
-  assert(dirs == ['e', 'se', 'ne', 'e'])
+      logging.debug("Tile {}: {}".format(tile.key(), tile.side))
 
 grid = HexGrid()
 
 for dir_list in my_input:
-  dirs = grid.minify_directions(grid.parse_directions(dir_list))
-  grid.flip(grid.centre, dirs)
-
-black_count = 0
+  dirs = grid.parse_directions(dir_list)
+  grid.flip(dirs)
 
 logging.debug("Grid:")
 grid.print()
 
+black_count = 0
 for tile in grid.all_tiles.values():
   #tile.print()
   if tile.side == BLACK:
