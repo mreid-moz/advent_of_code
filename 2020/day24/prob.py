d000a3810862fe9fd7a0a69ed1535f4197498e6d
Mark Reid <mreid@mozilla.com>
2020-12-25 23:20:15 -0400

    Solve day 24 part 2.
    Still very slow, but not as slow as the previous hash-based approach.

--- 2020/day24/prob.py
+++ 2020/day24/prob.py
@@ -33,29 +33,18 @@ if len(sys.argv) >= 2:
 with open(input_file) as fin:
   my_input = [l.strip().replace(' ', '') for l in fin.readlines()]
 
-class HexTile:
-  def __init__(self, x, y, z):
-    self.x = x
-    self.y = y
-    self.z = z
-    self.side = WHITE
-
-  def key(self):
-    return "{},{},{}".format(self.x, self.y, self.z)
-
-  def flip(self):
-    if self.side == WHITE:
-      self.side = BLACK
-      #logging.debug("Flipping tile {} from {} to {}".format(self.key(), WHITE, BLACK))
-    else:
-      self.side = WHITE
-      #logging.debug("Flipping tile {} from {} to {}".format(self.key(), BLACK, WHITE))
-
 class HexGrid:
   def __init__(self):
-    self.centre = HexTile(0, 0, 0)
-    self.all_tiles = {}
-    self.all_tiles[self.centre.key()] = self.centre
+    self.half_n = 120
+    n = self.half_n * 2
+    self.tiles = [[[False for k in range(n)] for j in range(n)] for i in range(n)]
+    self.black_tile_count = 0
+    self.min_flipped_x = None
+    self.min_flipped_y = None
+    self.min_flipped_z = None
+    self.max_flipped_x = None
+    self.max_flipped_y = None
+    self.max_flipped_z = None
 
   def parse_directions(self, dir_str):
     dir_list = list(dir_str)
@@ -70,126 +59,88 @@ class HexGrid:
       dirs.append(c)
     return dirs
 
-  def flip(self, directions):
+  def get_offset(self, x, y, z):
+    return [x + self.half_n, y + self.half_n, z + self.half_n]
+
+  def get_value(self, rx, ry, rz):
+    x, y, z = self.get_offset(rx, ry, rz)
+    return self.tiles[x][y][z]
+
+  def set_value(self, rx, ry, rz, v):
+    x, y, z = self.get_offset(rx, ry, rz)
+    self.tiles[x][y][z] = v
+
+  def flip(self, dir_list):
+    directions = self.parse_directions(dir_list)
     xd, yd, zd = [0, 0, 0]
     for d in directions:
       x, y, z = DIR_COORDS[d]
       xd += x
       yd += y
       zd += z
+    self.flip_one(xd, yd, zd)
 
-    t = HexTile(xd, yd, zd)
-    tk = t.key()
-    if tk in self.all_tiles:
-      self.all_tiles[tk].flip()
+  def flip_one(self, xd, yd, zd):
+    if self.min_flipped_x is None or xd < self.min_flipped_x:
+      self.min_flipped_x = xd
+    if self.min_flipped_y is None or yd < self.min_flipped_y:
+      self.min_flipped_y = yd
+    if self.min_flipped_z is None or zd < self.min_flipped_z:
+      self.min_flipped_z = zd
+    if self.max_flipped_x is None or xd > self.max_flipped_x:
+      self.max_flipped_x = xd
+    if self.max_flipped_y is None or yd > self.max_flipped_y:
+      self.max_flipped_y = yd
+    if self.max_flipped_z is None or zd > self.max_flipped_z:
+      self.max_flipped_z = zd
+
+    v = self.get_value(xd, yd, zd)
+    if v:
+      self.black_tile_count -= 1
     else:
-      t.flip()
-      self.all_tiles[tk] = t
+      self.black_tile_count += 1
 
-  def print(self):
-    for tile in sorted(self.all_tiles.values(), key=lambda x: x.x * 100000 + x.y * 1000 + x.z):
-      logging.debug("Tile {}: {}".format(tile.key(), tile.side))
+    self.set_value(xd, yd, zd, not v)
 
-  def fill_gaps(self):
-    min_x = None
-    max_x = None
-    min_y = None
-    max_y = None
-    min_z = None
-    max_z = None
-    for tile in grid.all_tiles.values():
-      if min_x is None or tile.x < min_x:
-        min_x = tile.x
-      if min_y is None or tile.y < min_y:
-        min_y = tile.y
-      if min_z is None or tile.z < min_z:
-        min_z = tile.z
-
-      if max_x is None or tile.x > max_x:
-        max_x = tile.x
-      if max_y is None or tile.y > max_y:
-        max_y = tile.y
-      if max_z is None or tile.z > max_z:
-        max_z = tile.z
-
-    for x in range(min_x, max_x+1):
-      for y in range(min_y, max_y+1):
-        for z in range(min_z, max_z+1):
-          t = HexTile(x, y, z)
-          k = t.key()
-          if k not in grid.all_tiles:
-            logging.debug("Adding missing tile {}".format(k))
-            grid.all_tiles[k] = t
-    logging.info("Found {} <= x <= {}, {} <= y <= {}, {} <= z <= {}".format(min_x, max_x, min_y, max_y, min_z, max_z))
-
+logging.debug("Initializing...")
 grid = HexGrid()
+logging.debug("Done initializing.")
 
 for dir_list in my_input:
-  dirs = grid.parse_directions(dir_list)
-  grid.flip(dirs)
+  grid.flip(dir_list)
 
-logging.debug("Grid:")
-grid.print()
-
-black_count = 0
-for tile in grid.all_tiles.values():
-  #tile.print()
-  if tile.side == BLACK:
-    black_count += 1
-
-logging.info("Part 1: Found {} black tiles".format(black_count))
+logging.info("Part 1: Found {} black tiles".format(grid.black_tile_count))
 
 
 def day(g):
-  flips = set()
-  all_values = [v for v in g.all_tiles.values()]
-  for t in all_values:
-    black_neighbours = 0
-    for d, [x, y, z] in DIR_COORDS.items():
-      new_tile = HexTile(t.x + x, t.y + y, t.z + z)
-      new_key = new_tile.key()
-      if new_key in g.all_tiles:
-        new_tile = g.all_tiles[new_key]
-      else:
-        g.all_tiles[new_key] = new_tile
-      #logging.debug("Checking {} neighbour of {} tile ({}) -> ({}) which was {}".format(d, t.side, t.key(), new_key, new_tile.side))
-      if new_tile.side == BLACK:
-        black_neighbours += 1
-    if t.side == BLACK and (black_neighbours == 0 or black_neighbours > 2):
-      logging.debug("Flipping ({}) from black to white".format(t.key()))
-      flips.add(t.key())
-    elif t.side == WHITE and black_neighbours == 2:
-      logging.debug("Flipping ({}) from white to black".format(t.key()))
-      flips.add(t.key())
+  flips = []
+  for x in range(g.min_flipped_x - 1, g.max_flipped_x + 2):
+    for y in range(g.min_flipped_y - 1, g.max_flipped_y + 2):
+      for z in range(g.min_flipped_z - 1, g.max_flipped_z + 2):
+        v = g.get_value(x, y, z)
+        #logging.debug("Checking ({},{},{}), current value is {}".format(x, y, z, v))
+        black_neighbours = 0
+        for [xd, yd, zd] in DIR_COORDS.values():
+          nv = g.get_value(x+xd, y+yd, z+zd)
+          if nv:
+            black_neighbours += 1
+        if v and (black_neighbours == 0 or black_neighbours > 2):
+          logging.debug("Flipping ({},{},{}) from black to white".format(x, y, z))
+          flips.append([x, y, z])
+        elif not v and black_neighbours == 2:
+          logging.debug("Flipping ({},{},{}) from white to black".format(x, y, z))
+          flips.append([x, y, z])
+
   logging.debug("Found {} flips.".format(len(flips)))
-  for flip in flips:
-    g.all_tiles[flip].flip()
+  for x, y, z in flips:
+    g.flip_one(x, y, z)
 
 
 
 for i in range(100):
-  #logging.debug("Before day {}:".format(i+1))
-  #grid.print()
-  grid.fill_gaps()
   day(grid)
-  #logging.debug("After day {}:".format(i+1))
-  #grid.print()
+  logging.info("Processed day {}, found {} black tiles".format(i+1, grid.black_tile_count))
 
-  black_count = 0
-  for tile in grid.all_tiles.values():
-    #tile.print()
-    if tile.side == BLACK:
-      black_count += 1
-  logging.info("Processed day {}, found {} black tiles".format(i+1, black_count))
-
-
-
-black_count = 0
-for tile in grid.all_tiles.values():
-  #tile.print()
-  if tile.side == BLACK:
-    black_count += 1
-
-logging.info("Part 2: Found {} black tiles".format(black_count))
+logging.info("Part 2: Found {} black tiles".format(grid.black_tile_count))
 
 # 4084 is too low.
