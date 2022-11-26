c72d6749f994099a30fd588a5f34a45715bc9fbe
Mark Reid <mreid@mozilla.com>
2022-11-25 20:07:03 -0400

    Solve day3 part 2

--- 2018/day3.py
+++ 2018/day3.py
@@ -2,7 +2,7 @@ from aocd.models import Puzzle
 from collections import defaultdict
 import re
 
-pattern = re.compile(r"#\d+ @ (\d+),(\d+): (\d+)x(\d+)")
+pattern = re.compile(r"#(\d+) @ (\d+),(\d+): (\d+)x(\d+)")
 
 p = Puzzle(year=2018, day=3)
 
@@ -18,10 +18,10 @@ for line in lines:
   if m is None:
     print(f"wonky input: {line}")
     break
-  left = int(m.group(1))
-  top = int(m.group(2))
-  width = int(m.group(3))
-  height = int(m.group(4))
+  left = int(m.group(2))
+  top = int(m.group(3))
+  width = int(m.group(4))
+  height = int(m.group(5))
 
   for w in range(width):
     for h in range(height):
@@ -34,3 +34,39 @@ for i in range(len(grid)):
       multi_claims += 1
 
 p.answer_a = multi_claims
+
+grid = []
+
+for i in range(1000):
+  grid.append([None]*1000)
+
+# Part B
+non_overlapping_codes = set()
+for line in lines:
+  m = pattern.match(line)
+  if m is None:
+    print(f"wonky input: {line}")
+    break
+  code = int(m.group(1))
+  left = int(m.group(2))
+  top = int(m.group(3))
+  width = int(m.group(4))
+  height = int(m.group(5))
+
+  overlapping = False
+  for w in range(width):
+    for h in range(height):
+      current = grid[w+left][h+top]
+      if current is None:
+        grid[w+left][h+top] = code
+      else:
+        overlapping = True
+        if current in non_overlapping_codes:
+          non_overlapping_codes.remove(current)
+  if not overlapping:
+    non_overlapping_codes.add(code)
+
+if len(non_overlapping_codes) == 1:
+  p.answer_b = non_overlapping_codes.pop()
+else:
+  print(f"Found {len(non_overlapping_codes)} non-overlapping codes")
