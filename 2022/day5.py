218fd87d53d2ec353869dc52e9bd7069bbdded8d
Mark Reid <mreid@mozilla.com>
2022-12-01 09:41:29 -0400

    Add template files for each day's problem

---
+++ 2022/day5.py
@@ -0,0 +1,15 @@
+from aocd.models import Puzzle
+from collections import defaultdict
+import logging
+import re
+import sys
+
+logging.basicConfig(level=logging.DEBUG)
+
+p = Puzzle(year=2022, day=5)
+
+lines = p.input_data.splitlines()
+
+#...
+
+# p.answer_a = 10
