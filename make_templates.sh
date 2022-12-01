2a03a701f545674b55c9266bf2869334492df657
Mark Reid <mreid@mozilla.com>
2022-12-01 09:40:39 -0400

    Add a script to generate templates for each day's problem

---
+++ make_templates.sh
@@ -0,0 +1,17 @@
+# Usage: for i in $(seq 1 25); do bash make_templates.sh $i; done
+
+echo "from aocd.models import Puzzle" >> day$1.py
+echo "from collections import defaultdict" >> day$1.py
+echo "import logging" >> day$1.py
+echo "import re" >> day$1.py
+echo "import sys" >> day$1.py
+echo "" >> day$1.py
+echo "logging.basicConfig(level=logging.DEBUG)" >> day$1.py
+echo "" >> day$1.py
+echo "p = Puzzle(year=2022, day=$1)" >> day$1.py
+echo "" >> day$1.py
+echo "lines = p.input_data.splitlines()" >> day$1.py
+echo "" >> day$1.py
+echo "#..." >> day$1.py
+echo "" >> day$1.py
+echo "# p.answer_a = 10" >> day$1.py
