f3e077c28e41be4928f9b45a831f40886e09e590
Mark Reid <mreid@mozilla.com>
2022-12-03 03:45:02 -0400

    Solve day 3

--- 2022/day3.py
+++ 2022/day3.py
@@ -10,6 +10,32 @@ p = Puzzle(year=2022, day=3)
 
 lines = p.input_data.splitlines()
 
-#...
+def priority(letter):
+  # ord('a') = 97
+  # ord('A') = 65
+  o = ord(letter)
+  if o < 97:
+    # uppercase
+    return o - 38
+  return o - 96
 
-# p.answer_a = 10
+total = 0
+for line in lines:
+  middle = int(len(line)/2)
+  left = line[:middle]
+  right = line[middle:]
+  common = set(left) & set(right)
+  total += priority(common.pop())
+
+p.answer_a = total
+
+total = 0
+logging.debug(f"Total lines: {len(lines)}")
+for i in range(0, len(lines), 3):
+  logging.debug(i)
+  one = set(lines[i])
+  two = one & set(lines[i+1])
+  three = two & set(lines[i+2])
+  total += priority(three.pop())
+
+p.answer_b = total
