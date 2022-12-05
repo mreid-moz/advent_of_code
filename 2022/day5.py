81e17b5b6964bde668c60290dcd381f6ec3543bc
Mark Reid <mreid@mozilla.com>
2022-12-05 09:01:48 -0400

    Solve d5p1

--- 2022/day5.py
+++ 2022/day5.py
@@ -10,6 +10,34 @@ p = Puzzle(year=2022, day=5)
 
 lines = p.input_data.splitlines()
 
-#...
+pattern = re.compile(r"move (\d+) from (\d) to (\d)")
 
-# p.answer_a = 10
+stacks = []
+for i in range(9):
+  stacks.append([])
+
+instructions = []
+
+for line in lines:
+  m = pattern.match(line)
+  if m:
+    count = int(m.group(1))
+    from_stack = int(m.group(2)) - 1
+    to_stack = int(m.group(3)) - 1
+    instructions.append((count, from_stack, to_stack))
+    continue
+  for i, idx in enumerate([1,5,9,13,17,21,25,29,33]):
+    if len(line) > idx and line[idx] != ' ':
+      stacks[i].insert(0, line[idx])
+
+for count, from_stack, to_stack in instructions:
+  for i in range(count):
+    crate = stacks[from_stack].pop()
+    stacks[to_stack].append(crate)
+
+tops = []
+for stack in stacks:
+  if len(stack) > 0:
+    tops.append(stack[-1])
+
+p.answer_a = "".join(tops)
