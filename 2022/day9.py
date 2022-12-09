3672eed25414df0a25f177a88146d99aaeeeed1b
Mark Reid <mreid@mozilla.com>
2022-12-09 09:26:01 -0400

    Solve d9p2

--- 2022/day9.py
+++ 2022/day9.py
@@ -4,7 +4,7 @@ import logging
 import re
 import sys
 
-logging.basicConfig(level=logging.DEBUG)
+logging.basicConfig(level=logging.INFO)
 
 p = Puzzle(year=2022, day=9)
 
@@ -20,17 +20,28 @@ lines = p.input_data.splitlines()
 #   "R 2",
 # ]
 
-start = (0,0)
+# lines = [
+#   "R 5",
+#   "U 8",
+#   "L 8",
+#   "D 3",
+#   "R 17",
+#   "D 10",
+#   "L 25",
+#   "U 20",
+# ]
+
+# part a: set to 1
+num_knots = 10
+knots = [(0,0)] * num_knots
 
 positions = set()
-head = start
-tail = start
-positions.add(start)
+positions.add(knots[0])
 
 for line in lines:
   direction, distance = line.split(' ')
   distance = int(distance)
-  logging.debug(f"Moving Head {direction} {distance} from {head}")
+  logging.debug(f"Moving Head {direction} {distance} from {knots[0]}")
   dx = 0
   dy = 0
   if direction == 'U':
@@ -44,31 +55,42 @@ for line in lines:
   else:
     logging.warn(f"Unexpected direction {direction}")
 
-  hx, hy = head
-  tx, ty = tail
   for i in range(distance):
+    hx, hy = knots[0]
     hx += dx
     hy += dy
     logging.debug(f"Moved head to {hx},{hy}")
-    head = (hx, hy)
-    if abs(hx - tx) > 1 or abs(hy - ty) > 1:
-      logging.debug(f"Moving tail from {tx},{ty} to catch up to {hx},{hy}")
-      # in a line, move ahead
-      if hx == tx:
-        ty += dy
-      elif hy == ty:
-        tx += dx
-      elif abs(hx - tx) > abs(hy - ty):
-        tx += dx
-        ty = hy
-      elif abs(hx - tx) < abs(hy - ty):
-        ty += dy
-        tx = hx
-      else:
-        logging.warn("Not sure how this is possible")
-      logging.debug(f"Tail moved to {tx},{ty} to catch up to {hx},{hy}")
-      tail = (tx,ty)
-      positions.add(tail)
+    knots[0] = (hx, hy)
+
+    for i in range(len(knots) - 1):
+      hx, hy = knots[i]
+      tx, ty = knots[i + 1]
+      knot_dx = hx - tx
+      knot_dy = hy - ty
+      if abs(knot_dx) > 1 or abs(knot_dy) > 1:
+        logging.debug(f"Moving knot {i+1} from {tx},{ty} to catch up to {hx},{hy}")
+        # in a line, move ahead
+        if hx == tx:
+          ty += knot_dy // 2
+        elif hy == ty:
+          tx += knot_dx // 2
+        elif abs(knot_dx) > abs(knot_dy): # Diagonal
+          tx += knot_dx // 2
+          ty = hy
+        elif abs(knot_dx) < abs(knot_dy):
+          ty += knot_dy // 2
+          tx = hx
+        elif abs(knot_dx) == abs(knot_dy):
+          # 2 away in both dirs
+          tx += knot_dx // 2
+          ty += knot_dy // 2
+        else:
+          logging.warning(f"Not sure how this is possible: dx={knot_dx}, dy={knot_dy}")
+        logging.debug(f"Knot moved to {tx},{ty} to catch up to {hx},{hy}")
+        knots[i+1] = (tx,ty)
+    # keep track of positions of the last knot only
+    positions.add(knots[-1])
 
 logging.info(f"Found {len(positions)} positions")
-p.answer_a = len(positions)
+#p.answer_a = len(positions)
+p.answer_b = len(positions)
