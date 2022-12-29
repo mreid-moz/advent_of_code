5ca95c1eaa2d40ef495ba837d23f1840cb0ec163
Mark Reid <mreid@mozilla.com>
2022-12-29 10:00:59 -0400

    make d19 faster!

--- 2022/day19.py
+++ 2022/day19.py
@@ -10,6 +10,7 @@ logging.basicConfig(level=logging.INFO)
 puzz = Puzzle(year=2022, day=19)
 
 TEST = False
+PART_ONE = False
 if TEST:
   lines = [
     "Blueprint 1: Each ore robot costs 4 ore. Each clay robot costs 2 ore. Each obsidian robot costs 3 ore and 14 clay. Each geode robot costs 2 ore and 7 obsidian.",
@@ -27,63 +28,86 @@ class Blueprint:
     if not m:
       logging.warning(f"Unexpected line: {line}")
     self.id = int(m.group(1))
-    self.costs = {}
-    # ore, clay, obsidian
-    self.costs['ore'] = (int(m.group(2)), 0, 0)
-    self.costs['clay'] = (int(m.group(3)), 0, 0)
-    self.costs['obsidian'] = (int(m.group(4)), int(m.group(5)), 0)
-    self.costs['geode'] = (int(m.group(6)), 0, int(m.group(7)))
+    self.ore_bot_ore = int(m.group(2))
+    self.clay_bot_ore = int(m.group(3))
+    self.obsidian_bot_ore = int(m.group(4))
+    self.obsidian_bot_clay = int(m.group(5))
+    self.geode_bot_ore = int(m.group(6))
+    self.geode_bot_obsidian = int(m.group(7))
 
 class State:
-  RESOURCE_TYPES = ['geode', 'obsidian', 'clay', 'ore']
-  def __init__(self, blueprint):
-    self.blueprint = blueprint
-    self.robots = defaultdict(int)
-    self.robots['ore'] = 1
-    self.resources = defaultdict(int)
-    self.time = 0
-    self.purchases = []
+  RESOURCE_TYPES = ('geode', 'obsidian', 'clay', 'ore')
+  def __init__(self, ore_bots=1, clay_bots=0, obsidian_bots=0, geode_bots=0,
+                     ore=0, clay=0, obsidian=0, geode=0, purchase=-1):
+    self.ore_bots = ore_bots
+    self.clay_bots = clay_bots
+    self.obsidian_bots = obsidian_bots
+    self.geode_bots = geode_bots
+    self.ore = ore
+    self.clay = clay
+    self.obsidian = obsidian
+    self.geode = geode
+    self.purchase = purchase
 
   def tick(self):
-    self.time += 1
-    for resource, count in self.robots.items():
-      self.resources[resource] += count
-      #logging.debug(f"Collected {count} {resource}. New total: {self.resources[resource]}")
-    for p in self.purchases:
-      self.robots[p] += 1
-    self.purchases = []
+    self.ore += self.ore_bots
+    self.clay += self.clay_bots
+    self.obsidian += self.obsidian_bots
+    self.geode += self.geode_bots
+    if self.purchase == 3:
+      self.ore_bots += 1
+    elif self.purchase == 2:
+      self.clay_bots += 1
+    elif self.purchase == 1:
+      self.obsidian_bots += 1
+    elif self.purchase == 0:
+      self.geode_bots += 1
+    self.purchase = -1
 
   def get_resources(self):
-    return [self.resources[t] for t in State.RESOURCE_TYPES]
+    return (self.geode, self.obsidian, self.clay, self.ore)
 
   def get_robots(self):
-    return [self.robots[t] for t in State.RESOURCE_TYPES]
+    return (self.geode_bots, self.obsidian_bots, self.clay_bots, self.ore_bots)
 
   def get_both(self):
-    return [(self.resources[t], self.robots[t]) for t in State.RESOURCE_TYPES]
+    return ((self.geode, self.geode_bots),(self.obsidian, self.obsidian_bots),(self.clay, self.clay_bots),(self.ore, self.ore_bots))
 
-  def get_affordables(self):
-    affordables = set()
-    for robot, costs in self.blueprint.costs.items():
-      ore, clay, obsidian = costs
-      if self.resources['ore'] >= ore and self.resources['clay'] >= clay and self.resources['obsidian'] >= obsidian:
-        affordables.add(robot)
+  def get_affordables(self, blueprint):
+    affordables = []
+    if self.ore >= blueprint.ore_bot_ore:
+      affordables.append(3)
+    if self.ore >= blueprint.clay_bot_ore:
+      affordables.append(2)
+    if self.ore >= blueprint.obsidian_bot_ore and self.clay >= blueprint.obsidian_bot_clay:
+      affordables.append(1)
+    if self.ore >= blueprint.geode_bot_ore and self.obsidian >= blueprint.geode_bot_obsidian:
+      affordables.append(0)
     return affordables
 
-  def buy(self, robot):
-    ore, clay, obsidian = self.blueprint.costs[robot]
-    logging.debug(f"Buying a {robot} robot for {ore} ore, {clay} clay and {obsidian} obsidian")
-    self.resources['ore'] -= ore
-    self.resources['clay'] -= clay
-    self.resources['obsidian'] -= obsidian
-    self.purchases.append(robot)
+  def buy(self, blueprint, robot):
+    ore_cost = 0
+    clay_cost = 0
+    obsidian_cost = 0
+    if robot == 0:
+      ore_cost = blueprint.geode_bot_ore
+      obsidian_cost = blueprint.geode_bot_obsidian
+    elif robot == 1:
+      ore_cost = blueprint.obsidian_bot_ore
+      clay_cost = blueprint.obsidian_bot_clay
+    elif robot == 2:
+      ore_cost = blueprint.clay_bot_ore
+    else:
+      ore_cost = blueprint.ore_bot_ore
+    logging.debug(f"Buying a {State.RESOURCE_TYPES[robot]} robot for {ore_cost} ore, {clay_cost} clay, {obsidian_cost} obsidian")
+    self.ore -= ore_cost
+    self.clay -= clay_cost
+    self.obsidian -= obsidian_cost
+    self.purchase = robot
 
   def clone(self):
-    cloned = State(self.blueprint)
-    cloned.robots = deepcopy(self.robots)
-    cloned.resources = deepcopy(self.resources)
-    cloned.purchases = deepcopy(self.purchases)
-    return cloned
+    return State(self.ore_bots, self.clay_bots, self.obsidian_bots, self.geode_bots,
+                 self.ore, self.clay, self.obsidian, self.geode, self.purchase)
 
   def __str__(self):
     return f"🤖={self.get_robots()}; ⛏️={self.get_resources()}"
@@ -94,80 +118,84 @@ for line in lines:
   blueprints.append(Blueprint(line))
 
 def max_geodes(blueprint, max_ticks=24):
-  outcomes = [State(blueprint)]
-  most_needed = {}
-  most_needed['ore'] = max([blueprint.costs[t][0] for t in State.RESOURCE_TYPES])
-  most_needed['clay'] = max([blueprint.costs[t][1] for t in State.RESOURCE_TYPES])
-  most_needed['obsidian'] = max([blueprint.costs[t][2] for t in State.RESOURCE_TYPES])
-  most_needed['geode'] = 10000000000
+  outcomes = [State()]
+  most_needed = [10000000000, blueprint.geode_bot_obsidian, blueprint.obsidian_bot_clay,
+                 max([blueprint.ore_bot_ore, blueprint.clay_bot_ore, blueprint.obsidian_bot_ore, blueprint.geode_bot_ore])]
   for i in range(max_ticks):
     logging.info(f"Time {i+1}, considering {len(outcomes)} states.")
-    #max_resources_so_far = outcomes[0].get_both()
-    #max_state = outcomes[0]
     new_outcomes = []
     for oi, outcome in enumerate(outcomes):
       if oi > 0 and oi % 100000 == 0:
         logging.info(f"Processing outcome {oi} of {len(outcomes)}")
-      #if outcome.get_both() > max_resources_so_far:
-      #  max_resources_so_far = outcome.get_both()
-      #  max_state = outcome
-      # If we can afford a geode robot, always buy it ASAP.
-      affordables = outcome.get_affordables()
-      # if 'geode' in affordables:
-      #   outcome.buy('geode')
-      #   new_outcomes.append(outcome)
-      # Always buy at least one clay robot ASAP too. Unless:
-      # If neither obsidian nor geode robots require clay, then this isn't important.
-      # I checked the input and they all require clay.
-      #elif 'clay' in affordables and outcome.robots['clay'] == 0 and something_needs_clay:
-      #  outcome.buy('clay')
-      #  new_outcomes.append(outcome)
-      # else:
-      # Otherwise, append all paths.
+        logging.info(f"State: {outcome}")
+      affordables = outcome.get_affordables(blueprint)
       # First, don't buy anything (unless *everything* is affordable, in which case don't wait)
       if len(affordables) < 4:
         logging.debug("Queueing next step: don't buy")
         new_outcomes.append(outcome)
+      # don't buy stuff on the last round.
+      if i + 1 == max_ticks:
+        continue
       # Then buy each of the possible things
-      for purchase in outcome.get_affordables():
-        if outcome.robots[purchase] >= most_needed[purchase]:
-          # don't buy more of this thing, we already get enough per turn.
-          logging.debug(f"Already have {outcome.robots[purchase]} {purchase} bots. Most we need is {most_needed[purchase]}. Not buying more.")
+      for purchase in affordables:
+        # don't buy more of the thing if we already get enough per turn.
+        if purchase == 1 and outcome.obsidian_bots > most_needed[purchase]:
+          logging.debug(f"Already have {outcome.obsidian_bots} obsidian bots. Most we need is {most_needed[purchase]}. Not buying more.")
           continue
+        elif purchase == 2 and outcome.clay_bots > most_needed[purchase]:
+          logging.debug(f"Already have {outcome.clay_bots} clay bots. Most we need is {most_needed[purchase]}. Not buying more.")
+          continue
+        elif purchase == 3 and outcome.ore_bots > most_needed[purchase]:
+          logging.debug(f"Already have {outcome.ore_bots} ore bots. Most we need is {most_needed[purchase]}. Not buying more.")
+          continue
+
         another = outcome.clone()
         logging.debug(f"Queueing next step: buy {purchase}")
-        another.buy(purchase)
+        another.buy(blueprint, purchase)
         new_outcomes.append(another)
     outcomes = []
     distinct_states = set()
     # Only keep one state for each distinct outcome. Blueprint is the same for all states.
     for o in new_outcomes:
       o.tick()
-      os = str(o)
+      # os = str(o)
+      os = o.get_both()
       if os not in distinct_states:
         outcomes.append(o)
         distinct_states.add(os)
-    # logging.info(f"In {len(new_outcomes)} there were {len(distinct_states)} distinct states")
-    # logging.info(f"So far, max resources was with {max_state}")
 
   logging.info(f"Checking {len(outcomes)} possible outcomes")
   max_geodes = 0
+  max_state = None
   for oi, o in enumerate(outcomes):
-    if oi > 0 and oi % 100000 == 0:
+    if oi > 0 and oi % 200000 == 0:
       logging.info(f"Processing outcome {oi} of {len(outcomes)}")
-    geodes = o.resources['geode']
+    geodes = o.geode
     if geodes > max_geodes:
       max_geodes = geodes
+      max_state = o
       logging.debug(f"Found a new max: {o}")
+  logging.info(f"Max state: {max_state}")
   return max_geodes
   #return max_state.resources['geode']
 
-quality_levels = 0
-for blueprint in blueprints:
-  m = max_geodes(blueprint)
-  logging.info(f"Max geodes for blueprint {blueprint.id}: {m}")
-  quality_levels += (m * blueprint.id)
+if PART_ONE:
+  quality_levels = 0
+  for blueprint in blueprints:
+    m = max_geodes(blueprint)
+    logging.info(f"Max geodes for blueprint {blueprint.id}: {m}")
+    quality_levels += (m * blueprint.id)
 
-logging.info(f"Quality level sum: {quality_levels}")
-if not TEST:
-  puzz.answer_a = quality_levels
+  logging.info(f"Quality level sum: {quality_levels}")
+  if not TEST:
+    puzz.answer_a = quality_levels
+else:
+  product = 1
+  for blueprint in blueprints[0:3]:
+    m = max_geodes(blueprint, max_ticks=32)
+    logging.info(f"Max geodes for blueprint {blueprint.id}: {m}")
+    product *= m
+
+  logging.info(f"Product was {product}")
+  if not TEST:
+    puzz.answer_b = product
