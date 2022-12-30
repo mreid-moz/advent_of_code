797f0f96a13c509ae1da63b01de483b0100a531f
Mark Reid <mreid@mozilla.com>
2022-12-30 11:51:44 -0400

    WIP

--- 2021/day22/prob.py
+++ 2021/day22/prob.py
@@ -21,11 +21,11 @@ def in_range(x, y, z):
   return in_range_one(x) and in_range_one(y) and in_range_one(z)
 
 class Cuboid:
-  match_expr = re.compile(r"(on|off) x=([0-9-]+)\.\.([0-9-]+),y=([0-9-]+)\.\.([0-9-]+),z=([0-9-]+)\.\.([0-9-]+)")
+  MATCH_EXPR = re.compile(r"(on|off) x=([0-9-]+)\.\.([0-9-]+),y=([0-9-]+)\.\.([0-9-]+),z=([0-9-]+)\.\.([0-9-]+)")
   def __init__(self, *args):
     if len(args) == 1:
       line = args[0]
-      m = Cuboid.match_expr.match(line)
+      m = Cuboid.MATCH_EXPR.match(line)
       if m:
         state_text = m.group(1)
         if state_text == 'on':
@@ -50,7 +50,10 @@ class Cuboid:
       self.z_max = args[6]
 
   def __str__(self):
-    return f"{self.state}: ({self.x_min, self.y_min, self.z_min})..({self.x_max, self.y_max, self.z_max}), volume {self.volume()}"
+    state_str = 'OFF'
+    if self.state == 1:
+      state_str = 'ON'
+    return f"{state_str}: ({self.x_min, self.y_min, self.z_min})..({self.x_max, self.y_max, self.z_max}), volume {self.volume()}"
 
   def set(self, state):
     for x in range(max(self.x_min, consider_min), min(self.x_max, consider_max) + 1):
@@ -58,8 +61,11 @@ class Cuboid:
         for z in range(max(self.z_min, consider_min), min(self.z_max, consider_max) + 1):
           state[(x, y, z)] = self.state
 
+  def nonempty(self):
+    return self.x_max > self.x_min and self.y_max > self.y_min and self.z_max > self.z_min
+
   def volume(self):
-    return (self.x_max - self.x_min) * (self.y_max - self.y_min) * (self.z_max - self.z_min)
+    return (self.x_max - self.x_min + 1) * (self.y_max - self.y_min + 1) * (self.z_max - self.z_min + 1)
 
   def overlaps_one(self, min1, max1, min2, max2):
     if min2 < min1:
@@ -71,23 +77,89 @@ class Cuboid:
     #logging.debug(f"{min1}..{max1} overlaps {min2}..{max2}")
     return True
 
+  def in_initialization_region(self):
+    init_region = Cuboid(1, -50, 50, -50, 50, -50, 50)
+    return self.overlaps(init_region)
+
   def overlaps(self, other):
-    x_overlaps = self.overlaps_one(self.x_min, self.x_max, other.x_min, other.x_max)
-    y_overlaps = self.overlaps_one(self.y_min, self.y_max, other.y_min, other.y_max)
-    z_overlaps = self.overlaps_one(self.z_min, self.z_max, other.z_min, other.z_max)
+    i = self.intersect(other)
+    return i.nonempty()
 
-    return x_overlaps and y_overlaps and z_overlaps
+    # x_overlaps = self.overlaps_one(self.x_min, self.x_max, other.x_min, other.x_max)
+    # y_overlaps = self.overlaps_one(self.y_min, self.y_max, other.y_min, other.y_max)
+    # z_overlaps = self.overlaps_one(self.z_min, self.z_max, other.z_min, other.z_max)
 
-  def minus(self, other, check_overlaps=True):
-    if check_overlaps:
-      if not self.overlaps(other):
-        return [self]
-    return []
+    # return x_overlaps and y_overlaps and z_overlaps
+
+  def intersect(self, other, state=1):
+    i_x_min = max(other.x_min, self.x_min)
+    i_x_max = min(other.x_max, self.x_max)
+
+    i_y_min = max(other.y_min, self.y_min)
+    i_y_max = min(other.y_max, self.y_max)
+
+    i_z_min = max(other.z_min, self.z_min)
+    i_z_max = min(other.z_max, self.z_max)
+    return Cuboid(state, i_x_min, i_x_max, i_y_min, i_y_max, i_z_min, i_z_max)
+
+  SUB_GRID = [ ((0,1),(0,1),(0,1)), ((1,2),(0,1),(0,1)), ((2,3),(0,1),(0,1)),
+               ((0,1),(1,2),(0,1)), ((1,2),(1,2),(0,1)), ((2,3),(1,2),(0,1)),
+               ((0,1),(2,3),(0,1)), ((1,2),(2,3),(0,1)), ((2,3),(2,3),(0,1)),
+
+               ((0,1),(0,1),(1,2)), ((1,2),(0,1),(1,2)), ((2,3),(0,1),(1,2)),
+               ((0,1),(1,2),(1,2)), ((1,2),(1,2),(1,2)), ((2,3),(1,2),(1,2)),
+               ((0,1),(2,3),(1,2)), ((1,2),(2,3),(1,2)), ((2,3),(2,3),(1,2)),
+
+               ((0,1),(0,1),(2,3)), ((1,2),(0,1),(2,3)), ((2,3),(0,1),(2,3)),
+               ((0,1),(1,2),(2,3)), ((1,2),(1,2),(2,3)), ((2,3),(1,2),(2,3)),
+               ((0,1),(2,3),(2,3)), ((1,2),(2,3),(2,3)), ((2,3),(2,3),(2,3)) ]
+
+  def subtract(self, other):
+    # if other.state == self.state:
+    #   return [self]
+
+    if not self.overlaps(other):
+      return [self]
+
+    # the bounding box is:
+    x_sorted = sorted([other.x_min, other.x_max, self.x_min, self.x_max])
+    y_sorted = sorted([other.y_min, other.y_max, self.y_min, self.y_max])
+    z_sorted = sorted([other.z_min, other.z_max, self.z_min, self.z_max])
+
+    x_ranges = [(x_sorted[0], x_sorted[1] - 1), (x_sorted[1], x_sorted[2] - 1), (x_sorted[2], x_sorted[3])]
+    y_ranges = [(y_sorted[0], y_sorted[1] - 1), (y_sorted[1], y_sorted[2] - 1), (y_sorted[2], y_sorted[3])]
+    z_ranges = [(z_sorted[0], z_sorted[1] - 1), (z_sorted[1], z_sorted[2] - 1), (z_sorted[2], z_sorted[3])]
+
+    remainder = []
+
+    for (x_min, x_max) in x_ranges:
+      for (y_min, y_max) in y_ranges:
+        for (z_min, z_max) in z_ranges:
+    # for ((xi0,xi1),(yi0,yi1),(zi0,zi1)) in Cuboid.SUB_GRID:
+    #   x_min = x_sorted[xi0]
+    #   x_max = x_sorted[xi1]
+    #   y_min = y_sorted[yi0]
+    #   y_max = y_sorted[yi1]
+    #   z_min = z_sorted[zi0]
+    #   z_max = z_sorted[zi1]
+          c = Cuboid(self.state, x_min, x_max, y_min, y_max, z_min, z_max)
+          if c.nonempty() and self.overlaps(c) and not other.overlaps(c):
+            remainder.append(c)
+    return remainder
+
+def total_ons(cuboids):
+  ons = 0
+  for c in cuboids:
+    if c.state == 1:
+      ons += c.volume()
+  return ons
 
 
 if __name__ == "__main__":
   cuboids = []
   for line in my_input:
+    if line.startswith('#'):
+      continue
     cuboids.append(Cuboid(line))
 
   #cubes = defaultdict(int)
@@ -100,24 +172,36 @@ if __name__ == "__main__":
   ## Next attempt:
   # if it's an on, find overlapping ons and find the diff, adding only the non-overlapping new on
   # if it's an off, find overlapping ons and subtract it from each one, re-adding leftover on bits
+  applied_cuboids = [cuboids[0]]
+  logging.info(f"Starting with {cuboids[0]}")
+  for cuboid in cuboids[1:]:
+    logging.info(f"Applying {cuboid}. Volume before: {total_ons(applied_cuboids)} (using {len(applied_cuboids)} cuboids)")
 
-  for cuboid in cuboids:
-    overlapping_cuboids = []
+    new_applied_cuboids = []
     for ac in applied_cuboids:
       if ac.overlaps(cuboid):
-        overlapping_cuboids.append(ac)
-    logging.debug(f"Found {len(overlapping_cuboids)} cuboids that overlap {cuboid}")
-    for oc in overlapping_cuboids:
-      applied_cuboids.remove(oc)
-    overlapping_cuboids.append(cuboid)
-    reconciled = reconcile(overlapping_cuboids)
-    logging.debug(f"Reconciled to {len(reconciled)} cuboids")
-    applied_cuboids += reconciled
+        logging.info(f"It overlaps with {ac}")
+        i = ac.intersect(cuboid)
+        logging.debug(f"Intersection: {i}. breaking down:")
+        for sub in ac.subtract(cuboid):
+          logging.debug(f"Remainder: {sub}")
+          new_applied_cuboids.append(sub)
+        # overlapping_cuboids.append(ac)
+      else:
+        # logging.debug(f"Doesn't overlap with {ac}")
+        new_applied_cuboids.append(ac)
+    if cuboid.state == 1:
+      logging.debug(f"Keeping entire new ON cuboid.")
+      new_applied_cuboids.append(cuboid)
+    applied_cuboids = new_applied_cuboids
+    logging.info(f"Volume after: {total_ons(applied_cuboids)} (using {len(applied_cuboids)} cuboids)")
 
-  total_ons = 0
-  for c in applied_cuboids:
-    if c.state == 1:
-      total_ons += c.volume()
+  # total_ons = 0
+  # for c in applied_cuboids:
+  #   if c.state == 1:
+  #     total_ons += c.volume()
 
-  logging.info(f"Total ons = {total_ons}")
+  logging.info(f"Total ons = {total_ons(applied_cuboids)}")
+
+  logging.info(f"Total ons in init region = {total_ons([c for c in applied_cuboids if c.in_initialization_region()])}")
 
