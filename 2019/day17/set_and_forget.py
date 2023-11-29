634f87bda3a1235d05b4095921222f4e440d06b2
hello@markreid.org <mreid@Marks-MacBook-Pro.local>
2023-11-28 20:30:50 -0400

    Solve 2019 day 17 part 2

--- 2019/day17/set_and_forget.py
+++ 2019/day17/set_and_forget.py
@@ -6,7 +6,20 @@ with open('input') as fin:
   program = fin.read().strip()
 instructions = [int(s) for s in program.split(",")]
 
+def to_ascii(stringy):
+  return [ord(c) for c in stringy]
+
+# Part 2:
+instructions[0] = 2
+
+scaffold_routine = to_ascii("A,B,B,A,C,A,C,A,C,B") + [10] + \
+                   to_ascii("L,6,R,12,R,8") + [10] + \
+                   to_ascii("R,8,R,12,L,12") + [10] + \
+                   to_ascii("R,12,L,12,L,4,L,4") + [10] + \
+                   to_ascii("n") + [10]
+
 computron = IntcodeComputer(instructions)
+computron.set_input(scaffold_routine)
 computron.run_program()
 
 def extract_map(computron):
@@ -35,8 +48,8 @@ def show_map(m):
     if y > my:
       my = y
 
-  for y in range(my):
-    for x in range(mx):
+  for y in range(my+1):
+    for x in range(mx+1):
       print(chr(m.get((x, y), 176)), end="")
     print("")
 
@@ -90,12 +103,55 @@ print("Example: {}".format(ap))
 assert(ap == 76)
 
 
-scaffold_map = extract_map(computron)
-show_map(scaffold_map)
-intersections = get_intersections(scaffold_map)
-for i in intersections:
-  scaffold_map[i] = ord('O')
-show_map(scaffold_map)
+# scaffold_map = extract_map(computron)
+# show_map(scaffold_map)
+# intersections = get_intersections(scaffold_map)
+# for i in intersections:
+#   scaffold_map[i] = ord('O')
+# show_map(scaffold_map)
 
-print("sum of alignment_parameters: {}".format(get_alignment_params(intersections)))
+# print("sum of alignment_parameters: {}".format(get_alignment_params(intersections)))
 
+# ..............DDDDD................................
+# ..............D...D................................
+# ....DDDDDDDDDDDDD.D................................
+# ....D.........D.D.D................................
+# DDDDDDDDDDDDD.DDDDDDD..............................
+# D...D.......D...D.D.D..............................
+# D...D.......D...D.D.D.....................DDDDDDDDD
+# D...D.......D...D.D.D.....................D.......D
+# DDDDD.......D...D.D.D.....................D.......D
+# ............D...D.D.D.....................D.......D
+# ....DDDDDDDDDDDDD.D.D.....................D.......D
+# ....D.......D.....D.D.....................D.......D
+# ....D.DDDDDDDDDDDDD.D.....................D.......D
+# ....D.D.....D.......D.....................D.......D
+# ....D.D.....D.....DDDDDDDDDDDDD.......DDDDDDDDDDDDD
+# ....D.D.....D.....D.D.........D.......D...D........
+# ....D.D.....DDDDDDDDD.........D.......D...D........
+# ....D.D...........D...........D.......D...D........
+# ....D.D.DDDDDDDDD.D...........D.......D...DDDDDD^..
+# ....D.D.D.......D.D...........D.......D............
+# ....D.DDDDDDDDDDDDD...........D.......D............
+# ....D...D.......D.............D.......D............
+# ....DDDDD.......D.............D.......D............
+# ................D.............D.......D............
+# ................D.............D.......D............
+# ................D.............D.......D............
+# ................D.............DDDDDDDDD............
+# ................D..................................
+# ................D..................................
+# ................D..................................
+# ................DDDDDDDDDDDDD......................
+
+
+
+# L,6,R,12,R,8,R,8,R,12,L,12,R,8,R,12,L,12,L,6,R,12,R,8,R,12,L,12,L,4,L,4,L,6,R,12,R,8,R,12,L,12,L,4,L,4,L,6,R,12,R,8,R,12,L,12,L,4,L,4,R,8,R,12,L,12
+
+# A=L,6,R,12,R,8
+# B=R,8,R,12,L,12
+# C=R,12,L,12,L,4,L,4
+# A,B,B,A,C,A,C,A,C,B
+
+
+print(computron.output[-1])
