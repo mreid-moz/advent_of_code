5096aea4282e782d683ec859a6ffb43355420a7e
Mark Reid <hello@markreid.org>
2023-12-03 10:46:06 -0400

    Add templates for 2023.

---
+++ 2023/day5.py
@@ -0,0 +1,20 @@
+from aocd.models import Puzzle
+from collections import defaultdict
+import logging
+import re
+import sys
+
+logging.basicConfig(level=logging.DEBUG)
+
+p = Puzzle(year=2023, day=5)
+
+TEST = False
+if TEST:
+    lines = p.examples[0].input_data.splitlines()
+else:
+    lines = p.input_data.splitlines()
+
+#...
+
+# if not TEST:
+#     p.answer_a = 10
