97f9fe13e36f2e6a0da33eee53573af18f559055
Mark Reid <hello@markreid.org>
2023-12-04 08:26:28 -0400

    Solve d4 part 1

--- 2023/day4.py
+++ 2023/day4.py
@@ -14,7 +14,22 @@ if TEST:
 else:
     lines = p.input_data.splitlines()
 
-#...
+card_values = 0
 
-# if not TEST:
-#     p.answer_a = 10
+for line in lines:
+    card_id, nums = line.split(': ')
+    winners, haves = nums.split(' | ')
+    winners = winners.strip()
+    haves = haves.strip()
+    win_set = set(winners.split())
+    have_set = set(haves.split())
+    num_winners = len(have_set.intersection(win_set))
+    card_value = 0
+    if num_winners > 0:
+        card_value = 2 ** (num_winners - 1)
+    logging.debug("{} contained {} winners for {} points.".format(card_id, num_winners, card_value))
+    card_values += card_value
+
+logging.info("Overall value: {}".format(card_values))
+if not TEST:
+    p.answer_a = card_values
