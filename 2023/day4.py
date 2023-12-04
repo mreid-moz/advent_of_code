8d1cfb4c9d879021e79fe7a5182ca7e8005d1c03
Mark Reid <hello@markreid.org>
2023-12-04 08:38:14 -0400

    Solve d4 part 2

--- 2023/day4.py
+++ 2023/day4.py
@@ -16,20 +16,29 @@ else:
 
 card_values = 0
 
-for line in lines:
+cards = [('Card 0', 0)] * len(lines)
+
+for i, line in enumerate(lines):
     card_id, nums = line.split(': ')
+    cards[i] = (card_id, cards[i][1] + 1)
     winners, haves = nums.split(' | ')
     winners = winners.strip()
     haves = haves.strip()
     win_set = set(winners.split())
     have_set = set(haves.split())
     num_winners = len(have_set.intersection(win_set))
+    for w in range(num_winners):
+        # add the number of this card to each of the next cards
+        cards[i+w+1] = (cards[i+w+1][0], cards[i+w+1][1] + cards[i][1])
     card_value = 0
     if num_winners > 0:
         card_value = 2 ** (num_winners - 1)
-    logging.debug("{} contained {} winners for {} points.".format(card_id, num_winners, card_value))
+    logging.debug("line {}: {} contained {} winners for {} points. we ended up with {} copies of it".format(i, card_id, num_winners, card_value, cards[i][1]))
     card_values += card_value
 
 logging.info("Overall value: {}".format(card_values))
+total_num_cards = sum([t[1] for t in cards])
+logging.info("Overall number of cards: {}".format(total_num_cards))
 if not TEST:
     p.answer_a = card_values
+    p.answer_b = total_num_cards
