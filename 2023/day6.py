14874655ffa50a03d8597b2f8bcfa4fe7e175afb
Mark Reid <hello@markreid.org>
2023-12-06 08:22:00 -0400

    Solve d6p1

--- 2023/day6.py
+++ 2023/day6.py
@@ -14,7 +14,30 @@ if TEST:
 else:
     lines = p.input_data.splitlines()
 
-#...
+times = [int(s) for s in lines[0].split()[1:]]
+distances = [int(s) for s in lines[1].split()[1:]]
 
-# if not TEST:
-#     p.answer_a = 10
+def ways_to_win(time, distance):
+    win_count = 0
+    for i in range(time):
+        # charge for i seconds
+        speed = i
+        my_distance = speed * (time - i)
+        logging.debug("t={}, d={}. Charging for {} -> {}".format(time, distance, i, my_distance))
+        if my_distance > distance:
+            win_count += 1
+    return win_count
+
+product = 1
+for i in range(len(times)):
+    time = times[i]
+    distance = distances[i]
+    logging.debug("Looking for ways to win with time {} and distance {}".format(time, distance))
+    w = ways_to_win(time, distance)
+    logging.debug("With time {} and distance {}: {}".format(time, distance, w))
+    product *= w
+    
+logging.info("Final product: {}".format(product))
+if not TEST:
+    p.answer_a = product
+
