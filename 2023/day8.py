1d45cd8f1bd5b4b03c407a98c0430148ff82fbf6
Mark Reid <hello@markreid.org>
2023-12-08 08:04:20 -0400

    Solve d8p1

--- 2023/day8.py
+++ 2023/day8.py
@@ -14,7 +14,25 @@ if TEST:
 else:
     lines = p.input_data.splitlines()
 
-#...
+moves = lines[0]
+num_moves = len(moves)
 
-# if not TEST:
-#     p.answer_a = 10
+def get_move(i):
+    return moves[i % num_moves]
+
+path = {}
+
+for line in lines[2:]:
+    lhs, rhs = line.split(' = ')
+    rl, rr = rhs[1:-1].split(', ')
+    path[lhs] = {'L': rl, 'R': rr}
+
+current = 'AAA'
+steps = 0
+while current != 'ZZZ':
+    next_direction = get_move(steps)
+    steps += 1
+    current = path[current][next_direction]
+
+if not TEST:
+    p.answer_a = steps
