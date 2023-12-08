f6daf49eb7c4e93a9d544e63b9d491f07e79151e
Mark Reid <hello@markreid.org>
2023-12-08 08:17:03 -0400

    Brute force for d8p2

--- 2023/day8.py
+++ 2023/day8.py
@@ -36,3 +36,24 @@ while current != 'ZZZ':
 
 if not TEST:
     p.answer_a = steps
+
+# Part 2
+currents = [k for k in path.keys() if k[-1] == 'A']
+num_nodes = len(currents)
+logging.info("Found {} nodes that end in A: {}".format(num_nodes, currents))
+num_zs = 0
+steps = 0
+while num_zs < num_nodes:
+    next_direction = get_move(steps)
+    steps += 1
+    num_zs = 0
+    for i in range(num_nodes):
+        next_node = path[currents[i]][next_direction]
+        currents[i] = next_node
+        if next_node[-1] == 'Z':
+            num_zs += 1
+    if steps % 100 == 0:
+        logging.debug("Step {}: {} Zs. {}".format(steps, num_zs, currents))
+
+if not TEST:
+    p.answer_b = steps
