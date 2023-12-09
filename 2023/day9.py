460eed2d026b23bef2da51bfaa351e511e503f42
Mark Reid <hello@markreid.org>
2023-12-09 08:05:54 -0400

    Solve day 9

--- 2023/day9.py
+++ 2023/day9.py
@@ -14,7 +14,53 @@ if TEST:
 else:
     lines = p.input_data.splitlines()
 
-#...
+def delta_sequence(s):
+    new_s = []
+    for i in range(len(s) - 1):
+        new_s.append(s[i+1] - s[i])
+    return new_s
 
-# if not TEST:
-#     p.answer_a = 10
+def show(s):
+    return ",".join([str(i) for i in s])
+
+def all_zero(s):
+    for o in s:
+        if o != 0:
+            return False
+    return True
+
+def get_components(s):
+    components = [s[-1]]
+    logging.debug("Getting components for {}. First component is {}".format(show(s), components[0]))
+    next_seq = delta_sequence(s)
+    while not all_zero(next_seq):
+        components.append(next_seq[-1])
+        logging.debug("Next sequence {}. Components are {}".format(show(next_seq), show(components)))
+        next_seq = delta_sequence(next_seq)
+    return components
+
+sequences = [[int(s) for s in line.split()] for line in lines]
+
+total = 0
+for s in sequences:
+    components = get_components(s)
+    logging.info("{} -> {}".format(show(s), show(components)))
+    total += sum(components)
+
+logging.info("Overall total: {}".format(total))
+
+if not TEST:
+    p.answer_a = total
+
+# part 2
+total = 0
+for s in sequences:
+    s.reverse()
+    components = get_components(s)
+    logging.info("{} -> {} -> {}".format(show(s), show(components), sum(components)))
+    total += sum(components)
+
+logging.info("Overall total: {}".format(total))
+
+if not TEST:
+    p.answer_b = total
